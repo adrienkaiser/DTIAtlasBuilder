77e30c5fa5fd7271a410fa6ed88515b94d48712c
adrienkaiser <adrien.kaiser@hotmail.fr>
2012-08-17 10:15:37 -0400

    ENH: Adding automatic configuration of softwares

--- ScriptWriter.h
+++ ScriptWriter.h
@@ -1,9 +1,11 @@
 #ifndef DEF_SCRIPTWRITER
 #define DEF_SCRIPTWRITER
 
+/*itk classes*/
 #include "itkImage.h"
 #include "itkImageFileReader.h"
 
+/*std classes*/
 #include <iostream>
 #include <string>
 #include <vector>
@@ -12,17 +14,22 @@
 class ScriptWriter
 {
 	public:
+/*WRITING FUNCTIONS*/
 		void WriteScript();
 		void Preprocess();
 		void AtlasBuilding();
 		void MainScript();
 
-		int setCroppingSize(); // returns 0 if no cropping , 1 if cropping needed
-		int CheckVoxelSize(); // returns 0 if voxel size OK , otherwise 1
-		int CheckSpaceDir(); // returns 0 if space dir OK , otherwise 1
+/*SCRIPT ACCESSORS*/
 		std::string getScript_Preprocess();
 		std::string getScript_AtlasBuilding();
 		std::string getScript_Main();
+
+/*CHECK DATASET*/
+		int setCroppingSize(); // returns 0 if no cropping , 1 if cropping needed
+		int CheckVoxelSize(); // returns 0 if voxel size OK , otherwise 1
+
+/*SET THE VALUES*/
 		void setCasesPath(std::vector < std::string > CasesPath);
 		void clearCasesPath();
 		void setOutputPath(std::string OutputPath);
@@ -33,10 +40,14 @@ class ScriptWriter
 		void setScaleLevels(std::vector< std::vector< double > > AtlasWerksScaleLevels);
 		void setInterpolType(std::string Type);
 		void setInterpolOption(std::string Option);
+		void setTensInterpol(std::string TensInterpol);
+		void setInterpolLogOption(std::string InterpolLogOption);
+		void setTensorTfm(std::string TensTfm);
 		void setAverageStatMethod(std::string Method);
 		void setSoftPath(std::vector < std::string > SoftPath);
 
 	private:
+/* VALUES */
 		std::vector < std::string > m_CasesPath;
 		std::string m_OutputPath;
 		int m_RegType; //=0 for using template and =1 for using existing first case
@@ -45,18 +56,20 @@ class ScriptWriter
 		std::string m_TemplatePath;
 		int m_CropSize [3]; //x,y,z
 		int m_NeedToBeCropped; //=0 if not and =1 if need to be cropped
-		int m_NeedToBeResampled; //=0 if not and =1 if need to be resampled
 		int m_Overwrite; //=0 if not and =1 if overwrite on existing files
 		std::vector< std::vector<double> > m_AtlasWerksScaleLevels; // contains X 2-int tables : [Scale,NbIter]
 		std::string m_InterpolType;
 		std::string m_InterpolOption;
+		std::string m_TensInterpol;
+		std::string m_InterpolLogOption;
+		std::string m_TensTfm;
 		std::string m_AverageStatMethod;
-	
+		std::vector < std::string > m_SoftPath; // contains the path to the softwares: 1=ImageMath, 2=ResampleDTIlogEuclidean, 3=CropDTI, 4=dtiprocess, 5=BRAINSFit, 6=AtlasWerks, 7=dtiaverage
+
+/* SCRIPTS */
 		std::string m_Script_Preprocess; //contains the whole Script for Pre processing
 		std::string m_Script_AtlasBuilding; //contains the whole Script for Atlas Building
 		std::string m_Script_Main;//contains the whole Main Scrit
-
-		std::vector < std::string > m_SoftPath; // contains the path to the softwares: 1=ImageMath, 2=ResampleDTIlogEuclidean, 3=CropDTI, 4=dtiprocess, 5=BRAINSFit, 6=AtlasWerks, 7=dtiaverage
 };
 #endif
 
