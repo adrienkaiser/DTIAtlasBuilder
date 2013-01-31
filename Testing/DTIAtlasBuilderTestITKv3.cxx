9d46313ddf9288e50c2d0e01d9c4d9b58a758147
adrienkaiser <akaiser@unc.edu>
2013-01-31 16:35:52 -0500

    COMP: Windows: Test executables tried to link with dynamic lib (dll) whereas the lib is static: remove dllimport

--- Testing/DTIAtlasBuilderTestITKv3.cxx
+++ Testing/DTIAtlasBuilderTestITKv3.cxx
@@ -1,12 +1,12 @@
 #include "itkTestMain.h"
 
 #ifdef WIN32
-#define MODULE_IMPORT __declspec(dllimport)
+#define MODULE_IMPORT// __declspec(dllimport) // "dllimport" -> Dll = Dynamic library on windows -> DTIAtlasBuilderLib is static, so just set MODULE_IMPORT to nothing: Fails to link (linkage error) if dllimport because library is static and tries to link as dynamic
 #else
 #define MODULE_IMPORT
 #endif
 
-extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char * []);
+extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char * []); // " extern "C" " so the function is linked in the lib like in C language i.e. the name is kept as is and not modified like in C++
 
 void RegisterTests()
 {
--- Testing/DTIAtlasBuilderTestITKv4.cxx
+++ Testing/DTIAtlasBuilderTestITKv4.cxx
@@ -1,10 +1,10 @@
 #ifdef WIN32
-#define MODULE_IMPORT __declspec(dllimport)
+#define MODULE_IMPORT// __declspec(dllimport) // "dllimport" -> Dll = Dynamic library on windows -> DTIAtlasBuilderLib is static, so just set MODULE_IMPORT to nothing: Fails to link (linkage error) if dllimport because library is static and tries to link as dynamic
 #else
 #define MODULE_IMPORT
 #endif
 
-extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char * []);
+extern "C" MODULE_IMPORT int ModuleEntryPoint(int, char * []); // " extern "C" " so the function is linked in the lib like in C language i.e. the name is kept as is and not modified like in C++
 
 int DTIAtlasBuilderTestITKv4(int argc, char* argv[])
 {
